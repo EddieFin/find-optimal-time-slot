636893b071e37012a5bfcbdf53f5bab9e023f85a
Eetu Mäkelä <makela.eetu@gmail.com>
2025-11-23 20:53:04 +0200

    Configure Terraform s3 backend

---
+++ infra/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket = "find-optimal-time-slot-terraform-backend"
+    key = "terraform.tfstate"
+    region = "eu-north-1"
+    encrypt = true
+    use_lockfile = true
+  }
+}
