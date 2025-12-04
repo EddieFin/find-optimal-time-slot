26fa0b7da43f074ed6c74ea01e568c24aee9cfdc
Eetu Mäkelä <makela.eetu@gmail.com>
2025-12-04 15:01:22 +0200

    Add missing path to API output in tf

--- infra/output.tf
+++ infra/output.tf
@@ -1,3 +1,4 @@
 output "api_endpoint" {
-  value = "${aws_api_gateway_stage.prod.invoke_url}/v1/meetings/optimize"
+  value       = "${aws_api_gateway_stage.prod.invoke_url}/api/v1/meetings/optimize"
+  description = "The public endpoint for the API"
 }
